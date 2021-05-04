e0d17bb3cb1a0dc47d28db81ba74b467a6db81fb
mdobaid311 <81803774+mdobaid311@users.noreply.github.com>
2021-05-04 21:08:57 +0530

    final commit

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
+const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
-const authorText=document.getElementById('author')
-const newQuoteBtn=document.getElementById('new-quote')
-const twitterBtn=document.getElementById('twitter')
+const authorText = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+const twitterBtn = document.getElementById("twitter");
+const loader = document.getElementById("loader");
 // Get Quotes From API
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+    loading()
   // Pick a random quote form apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.innerText = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+//   set Quote , hide loader
   quoteText.innerText = quote.text;
-  authorText.innerText=quote.author
-
+  complete()
 }
 
 async function getQuotes() {
+    loading()
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
@@ -26,7 +52,14 @@ async function getQuotes() {
   }
 }
 
-newQuoteBtn.addEventListener('click',newQuote)
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+twitterBtn.addEventListener("click", tweetQuote);
+newQuoteBtn.addEventListener("click", newQuote);
 
 // On Load
 getQuotes();
